fbcaba45c60efe1fc5fd58a16d156bbb5fa03ed5
Szabolcs-Zeg <110091769+Szabolcs-Zeg@users.noreply.github.com>
2023-03-02 14:20:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("id_hamburger").addEventListener("click", menusw)
+
+function menusw(){
+    document.getElementById("id_hamburger").classList.toggle("showedmenu");
+    document.getElementById("id_menu").classList.toggle("showedmenu");
+    document.getElementById("id_body").classList.toggle("showedmenu");
+    
+    document.body.main
+}
